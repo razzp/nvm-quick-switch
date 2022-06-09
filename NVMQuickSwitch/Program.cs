07d02e61a97c463c20170a7cce4efdef33dc82b4
razzp <64423130+razzp@users.noreply.github.com>
2022-06-09 21:31:51 +0100

    Ensure correct directory is set for relative paths

--- NVMQuickSwitch/Program.cs
+++ NVMQuickSwitch/Program.cs
@@ -5,6 +5,16 @@ namespace NVMQuickSwitch
         [STAThread]
         static void Main()
         {
+            var fileInfo = new FileInfo(Application.ExecutablePath);
+
+            if (fileInfo.DirectoryName is not null)
+            {
+                // If the app is launched via the registry then the current directory is
+                // changed to `C:\Windows\System32`, which breaks all the relative paths.
+                // I'm not sure why this happens, but here's the solution.
+                Directory.SetCurrentDirectory(fileInfo.DirectoryName);
+            }
+
             ApplicationConfiguration.Initialize();
             Application.Run(new QuickSwitchApp());
         }
